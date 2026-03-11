5c86b7bdccf6d90d6e0d25bcdf9dafc6305f5f52
EnochXiong <enochtxiong@gmail.com>
2026-03-11 15:56:46 -0500

    Create Enoch Xiong Program 1.py

---
+++ Enoch Xiong Program 1.py
@@ -0,0 +1,16 @@
+def initials_generator(personsName):
+
+    personsInitials = ""
+    name_parts = personsName.split()
+
+    for name in name_parts:
+        personsInitials += name[0].upper() + ". "
+
+    return personsInitials.strip()
+
+if __name__=="__main__":
+    personsName = input('Enter the users first, middle, and last name: ')
+
+    initials = initials_generator(personsName)
+
+    print(initials)
